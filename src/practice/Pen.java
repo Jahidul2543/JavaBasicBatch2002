11f903c6a4e52f415c8aae57fd3c62a24d5a522f
jahidul2543 <jahidul2543@gmail.com>
2020-08-03 21:34:39 -0400

    class15 java 05

--- src/practice/Pen.java
+++ src/practice/Pen.java
@@ -1,10 +1,12 @@
+package practice;
+
 public class Pen {
 
     public static void main(String[] args){
 
         // Write the name of the desgin(class name) nameOfYourObject = Assign someone to build the object using the
-        // desgin Pen()
-        // Pen() --> Constructor --> Is capable to create Object in Java
+        // desgin practice.Pen()
+        // practice.Pen() --> Constructor --> Is capable to create Object in Java
         // new Java reserved key word
 
         Pen  myPen = new Pen();
